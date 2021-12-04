0cebfba0bce8e6f6fcb46e8257209189d5874b7d
TahirReyaz <tahir200213@st.jmi.ac.in>
2021-12-04 18:12:32 +0530

    Basic setup and sent notification on android

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
 export default function App() {
+  const triggerNotifHandler = () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Yo!!',
+        body: 'I notified you'
+      },
+      trigger: {
+        seconds: 3
+      }
+    })
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Don't Push" onPress={triggerNotifHandler} />
     </View>
   );
 }
